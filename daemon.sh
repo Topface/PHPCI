cb90a5a5d0c3accee56fc45557302ea4e9c088b2
Stan Gumeniuk <s.gumeniuk@topface.com>
2014-10-21 13:19:48 +0400

    [add] git clone error fix

--- daemon.sh
+++ daemon.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
+
+sudo -u www-data -H sh -c "cd /phpci && git config --global core.compression 0"
 while true; do
 
 if ps -ef |grep -v grep |grep "phpci:run-builds"; then
         echo "nothing todo"
 else
     echo "starting phpci cron"
-    /phpci/console phpci:run-builds
+    sudo -u www-data -H sh -c "/phpci/console phpci:run-builds"
 fi
   sleep 15
 done
