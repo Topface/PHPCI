233aaa8f57730c92ca5f6d1a0cfa146c6939eaea
Stan Gumeniuk <s.gumeniuk@topface.com>
2014-10-21 13:22:00 +0400

    [add] phpcsdiff ks

---
+++ public/assets/js/build-plugins/phpcsdiff.js
@@ -0,0 +1,71 @@
+var phpcsdiffPlugin = PHPCI.UiPlugin.extend({
+    id: 'build-phpcsdiff',
+    css: 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
+    title: 'PHP Code Sniffer Diff',
+    lastData: null,
+    box: true,
+    rendered: false,
+
+    register: function() {
+        var self = this;
+        var query = PHPCI.registerQuery('phpcsdiff-data', -1, {key: 'phpcsdiff-data'})
+
+        $(window).on('phpcsdiff-data', function(data) {
+            self.onUpdate(data);
+        });
+
+        $(window).on('build-updated', function() {
+            if (!self.rendered) {
+                query();
+            }
+        });
+    },
+
+    render: function() {
+        return $('<table class="table table-striped" id="phpcsdiff-data">' +
+            '<thead>' +
+            '<tr>' +
+            '   <th>File</th>' +
+            '   <th>Line</th>' +
+            '   <th>Message</th>' +
+            '</tr>' +
+            '</thead><tbody></tbody></table>');
+    },
+
+    onUpdate: function(e) {
+        if (!e.queryData) {
+            return;
+        }
+
+        this.rendered = true;
+        this.lastData = e.queryData;
+
+        var errors = this.lastData[0].meta_value;
+        var tbody = $('#phpcsdiff-data tbody');
+        tbody.empty();
+
+        for (var i in errors) {
+            var file = errors[i].file;
+
+            if (PHPCI.fileLinkTemplate) {
+                var fileLink = PHPCI.fileLinkTemplate.replace('{FILE}', file);
+                fileLink = fileLink.replace('{LINE}', errors[i].line);
+
+                file = '<a target="_blank" href="'+fileLink+'">' + file + '</a>';
+            }
+
+            var row = $('<tr>' +
+                '<td>'+file+'</td>' +
+                '<td>'+errors[i].line+'</td>' +
+                '<td>'+errors[i].message+'</td></tr>');
+
+            if (errors[i].type == 'ERROR') {
+                row.addClass('danger');
+            }
+
+            tbody.append(row);
+        }
+    }
+});
+
+PHPCI.registerPlugin(new phpcsdiffPlugin());
