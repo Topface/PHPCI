43c639ff6ceddacd981cf34d3b68bf67975a2a4f
Stan Gumeniuk <s.gumeniuk@topface.com>
2014-12-25 15:38:21 +0300

    [fix]

---
+++ public/assets/js/build-plugins/phpunitdiffs.js
@@ -0,0 +1,71 @@
+var phpunitdiffsPlugin = PHPCI.UiPlugin.extend({
+    id: 'build-phpunitdiffs-errors',
+    css: 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
+    title: 'PHPUnit',
+    lastData: null,
+    displayOnUpdate: false,
+    box: true,
+    rendered: false,
+
+    register: function() {
+        var self = this;
+        var query = PHPCI.registerQuery('phpunitdiffs-data', -1, {key: 'phpunitdiffs-data'})
+
+        $(window).on('phpunitdiffs-data', function(data) {
+            self.onUpdate(data);
+        });
+
+        $(window).on('build-updated', function() {
+            if (!self.rendered) {
+                self.displayOnUpdate = true;
+                query();
+            }
+        });
+    },
+
+    render: function() {
+
+        return $('<table class="table table-striped" id="phpunitdiffs-data">' +
+            '<thead>' +
+            '<tr>' +
+            '   <th>Test</th>' +
+            '</tr>' +
+            '</thead><tbody></tbody></table>');
+    },
+
+    onUpdate: function(e) {
+        if (!e.queryData) {
+            return;
+        }
+
+        this.rendered = true;
+        this.lastData = e.queryData;
+
+        var tests = this.lastData[0].meta_value;
+        var tbody = $('#phpunitdiffs-data tbody');
+        tbody.empty();
+
+        for (var i in tests) {
+
+            var row = $('<tr>' +
+                '<td><strong>'+tests[i].suite+'' +
+                '::'+tests[i].test+'</strong><br>' +
+                ''+(tests[i].message || '')+'</td>' +
+                '</tr>');
+
+            if (!tests[i].pass) {
+                row.addClass('danger');
+            } else {
+                if (tests[i].skippedinci){
+                    row.addClass('warning');
+                } else {
+                    row.addClass('success');
+                }
+            }
+
+            tbody.append(row);
+        }
+    }
+});
+
+PHPCI.registerPlugin(new phpunitdiffsPlugin());
